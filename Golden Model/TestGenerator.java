b1cacaeecab82c91987b8995930d8d024c9117c5
sepehrAmini <sepehramini83@gmail.com>
2020-07-22 19:03:03 +0430

    test1

--- Golden Model/TestGenerator.java
+++ Golden Model/TestGenerator.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class TestGenerator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = 2000;
+        int n = 1000;
         Random random = new Random();
         scanner.close();
         try {
@@ -15,21 +15,33 @@ public class TestGenerator {
             BufferedWriter bufferedWriter = new BufferedWriter(writer);
             for (int i = 0; i < n; i++) {
                 int random_int = random.nextInt()% 65536;
-                if(random_int < 0)
-                    random_int *= -1;
-                StringBuilder stringBuilder = new StringBuilder();
-                for (int j = 0; j < 16; j++) {
-                    char c = (random_int%2 == 0)? '0' : '1';
-                    random_int /= 2;
-                    stringBuilder.append(c);
-                }
-                stringBuilder.reverse();
-                bufferedWriter.write(stringBuilder.toString());
+                int random_int2 = random.nextInt() % 65536;
+                int random_int3 = random.nextInt() % 804;
+
+                random_int = random_int < 0 ? -random_int : random_int;
+                random_int2 = random_int2 < 0 ? -random_int2 : random_int2;
+                random_int3 = random_int3 < 0 ? -random_int3 : random_int3;
+
+                bufferedWriter.write(getStringFromInt(random_int));
+                bufferedWriter.newLine();
+                bufferedWriter.write(getStringFromInt(random_int2));
+                bufferedWriter.newLine();
+                bufferedWriter.write(getStringFromInt(random_int3));
                 bufferedWriter.newLine();
             }
             bufferedWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static String getStringFromInt(int random_int) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int j = 0; j < 16; j++) {
+            char c = (random_int%2 == 0)? '0' : '1';
+            random_int /= 2;
+            stringBuilder.append(c);
+        }
+        return stringBuilder.reverse().toString();
     }
 }
